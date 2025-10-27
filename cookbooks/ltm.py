3e61571d9d519dbeed369f478cc88146e6eb5319
He Fei <droxer.he@gmail.com>
2025-10-27 11:29:10 +0800

    feat: add long term memories.

---
+++ cookbooks/ltm.py
@@ -0,0 +1,51 @@
+from langchain.embeddings import init_embeddings
+from langchain.chat_models import init_chat_model
+from langgraph.store.base import BaseStore
+from langgraph.store.memory import InMemoryStore
+from langgraph.graph import START, MessagesState, StateGraph
+
+from rich import print
+
+from dotenv import load_dotenv
+load_dotenv()
+
+model = init_chat_model("openai:gpt-4o-mini")
+
+# Create store with semantic search enabled
+embeddings = init_embeddings("openai:text-embedding-3-small")
+store = InMemoryStore(
+    index={
+        "embed": embeddings,
+        "dims": 1536,
+    }
+)
+
+store.put(("user_123", "memories"), "1", {"text": "I love noodles"})
+store.put(("user_123", "memories"), "2", {"text": "I am a plumber"})
+
+def chat(state, *, store: BaseStore):
+    # Search based on user's last message
+    items = store.search(
+        ("user_123", "memories"), query=state["messages"][-1].content, limit=2
+    )
+    memories = "\n".join(item.value["text"] for item in items)
+    memories = f"## Memories of user\n{memories}" if memories else ""
+    response = model.invoke(
+        [
+            {"role": "system", "content": f"You are a helpful assistant.\n{memories}"},
+            *state["messages"],
+        ]
+    )
+    return {"messages": [response]}
+
+
+builder = StateGraph(MessagesState)
+builder.add_node(chat)
+builder.add_edge(START, "chat")
+graph = builder.compile(store=store)
+
+for message, metadata in graph.stream(
+    input={"messages": [{"role": "user", "content": "I'm hungry"}]},
+    stream_mode="messages",
+):
+    print(message.content, end="")
